dc0973c84042b8a6683e931faefb3b00e124a298
ripunk <30873947+ripunk@users.noreply.github.com>
2020-06-06 21:59:35 +0700

    for termux

--- aa.sh
+++
@@ -1,5 +0,0 @@
-# cie ngintip
-
-z="
-";cz='="';hz='p $2';rz='i Ke';Kz='34m'\''';nz='Ada"';Pz=' #YE';Yz='r';Gz='32m'\''';Rz='C='\''\';iz='}';gz='slee';lz='n Ti';Bz='e[1;';Wz='(){';jz=' "Pi';Dz=' #RE';Hz=' #GR';Fz='G='\''\';fz='"';Sz='36m'\''';Lz=' #BL';Mz='UE';tz='u" 2';Xz='clea';sz=' Men';Oz='33m'\''';pz=' "Ke';Zz='echo';ez=' "$1';dz='date';mz='dak ';Jz='B='\''\';az=' "==';Iz='EEN';Ez='D';Uz='AN';Nz='Y='\''\';kz='liha';bz='====';Az='R='\''\';Vz='func';Tz=' #CY';oz=' 2';Qz='LLOW';qz='mbal';Cz='31m'\''';
-eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Bz$Gz$Hz$Iz$z$Jz$Bz$Kz$Lz$Mz$z$Nz$Bz$Oz$Pz$Qz$z$Rz$Bz$Sz$Tz$Uz$z$Vz$Wz$z$Xz$Yz$z$Zz$az$bz$bz$bz$bz$bz$bz$bz$cz$z$dz$z$Zz$az$bz$bz$bz$bz$bz$bz$bz$cz$z$Zz$ez$fz$z$gz$hz$z$iz$z$Vz$jz$kz$lz$mz$nz$oz$z$Vz$pz$qz$rz$sz$tz"
